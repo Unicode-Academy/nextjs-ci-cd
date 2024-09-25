6227f536d16e893f6ec6cbf4d2424b28c69cf3fc
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-09-25 23:03:03 +0700

    Update test 4

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,9 +5,9 @@ type Params = {
 };
 
 export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` };
+  return { title: `Post 1: ${params.slug}` };
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>;
+  return <h1>Slug 1: {params.slug}</h1>;
 }
