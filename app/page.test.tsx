e9d0ceb212bb5171d4d185b45abb9f5c5eb83a33
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-09-25 17:14:19 +0700

    Update test 02

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -6,5 +6,5 @@ import Page from "./page";
 
 it("App Router: Works with Server Components", () => {
   render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Unicode Academy");
+  expect(screen.getByRole("heading")).toHaveTextContent("Unicode Academy1");
 });
